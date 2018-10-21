1284796dfeea1c8caff7cd6653509486f96af704
Karmaliu <2134515173@qq.com>
2018-10-21 09:13:25 +0600

    17

--- ex17.py
+++ ex17.py
@@ -2,7 +2,7 @@ class MyStuff(object):
     
     def __init__(self):
         # __init__ 实例化对象调用这个函数，对你创建的空对象实现了初始化
-        # self 是空对象可以对它进行类似模块，字典等操作，为它设置变量 
+        # self 是空对象可以对它进行类似模块，字典等操作，为它设置变量u 
         self.tangerine = "And now a thousand years between"
         
     def apple(self):

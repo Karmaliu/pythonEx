80afc99674fa266c60a9e3234aa811b7991e76b1
Karmaliu <2134515173@qq.com>
2018-10-21 14:25:39 +0600

    room

---
+++ projects/ex20/setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import settup
+except ImportError:
+    from distutils.core import setup
+    
+    config = {
+        'description': 'My Project',
+        'author': 'L',
+        'url': 'URL to get it at.',
+        'version': '0.1',
+        'install_requires': ['nose'],
+        'packages': ['EX20'],
+        'scripts': [],
+        'name': 'projectname'
+    }
+
+setup(**config)
